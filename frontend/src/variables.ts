4edb524cdffa444e3fe700ae31241af0ddef4db9
Sean Culloden <macbookpreaux@MacBook-Preaux.local>
2023-04-15 13:54:41 +0200

    vercel

--- frontend/src/variables.ts
+++ frontend/src/variables.ts
@@ -1,2 +1,3 @@
 export const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID || ""
 export const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY || ""
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8000/'
